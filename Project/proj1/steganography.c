f72f02106caa2a68b89c7d9aa94d1ea3e4b67321
Kevin <cookblob@gmail.com>
2026-01-15 21:11:27 +0800

    implement function steganography

--- Project/proj1/steganography.c
+++ Project/proj1/steganography.c
@@ -19,15 +19,37 @@
 #include "imageloader.h"
 
 //Determines what color the cell at the given row/col should be. This should not affect Image, and should allocate space for a new Color.
-Color *evaluateOnePixel(Image *image, int row, int col)
+Color evaluateOnePixel(Image *image, int row, int col)
 {
-	//YOUR CODE HERE
+    Color pixel;
+    uint8_t B_value = image->image[row][col].B;
+    if (B_value & 1) {
+        pixel.R = 255;
+        pixel.G = 255;
+        pixel.B = 255;
+    } else {
+        pixel.R = 0;
+        pixel.G = 0;
+        pixel.B = 0;
+    }
+    return pixel;
 }
 
 //Given an image, creates a new image extracting the LSB of the B channel.
 Image *steganography(Image *image)
 {
-	//YOUR CODE HERE
+    Image *out_image = malloc(sizeof(Image));
+    out_image->image = malloc((image->rows) * sizeof(Color*));
+    out_image->cols = image->cols;
+    out_image->rows = image->rows;
+    for (int i = 0; i < out_image->rows; i++) {
+        out_image->image[i] = malloc((out_image->cols)*sizeof(Color));
+        for (int j = 0; j < out_image->cols; j++) {
+            out_image->image[i][j] = evaluateOnePixel(image, i, j);
+        }
+    }
+
+    return out_image;
 }
 
 /*
@@ -45,5 +67,12 @@ Make sure to free all memory before returning!
 */
 int main(int argc, char **argv)
 {
-	//YOUR CODE HERE
+    Image *in_image = readData(argv[1]);
+    if (in_image == NULL) {
+        return -1;
+    }
+    Image *out_image = steganography(in_image); 
+    writeData(out_image);
+    freeImage(in_image);
+    freeImage(out_image);
 }
