f93633df536cc6ac732223b614d0f85d34dbe848
Kevin <cookblob@gmail.com>
2026-03-15 18:19:52 +0800

    implement evaluateOneCell

--- Project/proj1/gameoflife.c
+++ Project/proj1/gameoflife.c
@@ -8,7 +8,7 @@
 **				@xllzi
 **
 **
-** DATE:        2025-12-09
+** DATE:        2026-03-15
 **
 **************************************************************************/
 
@@ -20,9 +20,37 @@
 //Determines what color the cell at the given row/col should be. This function allocates space for a new Color.
 //Note that you will need to read the eight neighbors of the cell in question. The grid "wraps", so we treat the top row as adjacent to the bottom row
 //and the left column as adjacent to the right column.
-Color *evaluateOneCell(Image *image, int row, int col, uint32_t rule)
+Color* evaluateOneCell(Image *image, int row, int col, uint32_t rule)
 {
-	//YOUR CODE HERE
+    Color* pixel = &(image->image[row][col]);
+    // count the number of lived neighbours
+    // (x-1, y-1), (x, y-1), (x+1, y-1) ...
+    uint32_t old_state_R = !!(pixel->R);
+    uint32_t old_state_G = !!(pixel->G);
+    uint32_t old_state_B = !!(pixel->B);
+    uint32_t n_lived_R = 0;
+    uint32_t n_lived_G = 0;
+    uint32_t n_lived_B = 0;
+    int x_bound = image->cols;
+    int y_bound = image->rows;
+    for (int dx = -1; dx <= 1; dx++) {
+        for (int dy = -1; dy <= 1; dy++) {
+            if (dx == 0 & dy == 0) continue;
+            Color* neighbor = &(image->image[(row+dy)%y_bound][(col+dx)%x_bound]);
+            n_lived_R += !!(neighbor->R);
+            n_lived_G += !!(neighbor->G);
+            n_lived_B += !!(neighbor->B);
+        }
+    }
+
+    uint32_t which_bit;
+    which_bit = n_lived_R + old_state_R * 9;
+    pixel->R = (rule > which_bit & 1)*255;
+    which_bit = n_lived_G + old_state_G * 9;
+    pixel->G = (rule > which_bit & 1)*255;
+    which_bit = n_lived_B + old_state_B * 9;
+    pixel->B = (rule > which_bit & 1)*255;
+    return pixel;
 }
 
 //The main body of Life; given an image and a rule, computes one iteration of the Game of Life.
